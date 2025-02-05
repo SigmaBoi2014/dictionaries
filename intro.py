d68d8c392906ee7c7e67822a60d3aa59327a9833
SigmaBoi2014 <bernardo2014mendes@gmail.com>
2025-02-05 07:53:03 -0800

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,8 @@
+capitals={}
+capitals["canada"]="ottawa"
+capitals["Brazil"]="brasilia"
+print(capitals)
+print(list(capitals.keys()))
+print(list(capitals.values()))
+del(capitals["canada"])
+print(capitals)
